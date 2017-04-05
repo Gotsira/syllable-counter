cca680ab8e255ce8d2bf581a3f4ad3a88e5b6d05
Sirasath Piyapootinun <gotmadagascar@gmail.com>
2017-04-06 02:21:41 +0700

    Updated gitignore

---
+++ unit-converter/src/ku/util/UnitType.java
@@ -0,0 +1,29 @@
+package ku.util;
+
+/**
+ * Unit types for conversion.
+ * 
+ * @author Sirasath Piyapootinun
+ *
+ */
+public enum UnitType {
+	/**
+	 * Length units.
+	 */
+	Length,
+	
+	/**
+	 * Area units.
+	 */
+	Area,
+	
+	/**
+	 * Weight units.
+	 */
+	Weight,
+	
+	/**
+	 * Volume units.
+	 */
+	Volume;
+}
