a2616c9201d87dad85011384ab2c754840dce5ab
Sirasath Piyapootinun <gotmadagascar@gmail.com>
2017-04-06 02:10:22 +0700

    Update javadoc

--- syllable-counter/src/OOSyllableCounter/WordCounter.java
+++ syllable-counter/src/OOSyllableCounter/WordCounter.java
@@ -1,5 +1,12 @@
 package OOSyllableCounter;
 
+/**
+ * WordCounter class for counting syllable in a word using State Design Pattern
+ * (Object-Oriented).
+ * 
+ * @author Sirasath Piyapootinun
+ *
+ */
 public class WordCounter {
 	private final State START = new StartState();
 	private final State SINGLEVOWEL = new SingleVowelState();
@@ -12,78 +19,94 @@ public class WordCounter {
 	private int pointer;
 	private int syllableCount = 0;
 
+	/**
+	 * Sets the state of the WordCounter.
+	 * 
+	 * @param newstate
+	 *            is the state to be set.
+	 */
 	public void setState(State newstate) {
 		this.state = newstate;
 	}
-	
-	public static void main(String[] args) {
-		WordCounter w = new WordCounter();
-		System.out.println(w.countSyllables("home-brew"));
-	}
-	
+
+	/**
+	 * Return the total number of syllables counted in a word.
+	 * 
+	 * @param word
+	 *            is the word to be counted.
+	 * @return the number of syllables in a word.
+	 */
 	public int countSyllables(String word) {
 		setState(START);
 		syllableCount = 0;
 		this.word = word;
-		for(int i = 0; i < word.length(); i++) {
+		for (int i = 0; i < word.length(); i++) {
 			char c = word.charAt(i);
-			if(c == '\'') {
+			if (c == '\'') {
 				continue;
 			}
 			pointer = i;
 			this.state.handleChar(c);
-			if(state == NONWORD) {
+			if (state == NONWORD) {
 				return 0;
 			}
 		}
 		return syllableCount;
 	}
+
+	/**
+	 * Starting state of the WordCounter.
+	 * 
+	 * @author Sirasath Piyapootinun
+	 *
+	 */
 	class StartState extends State {
-		
+
 		@Override
 		public void handleChar(char c) {
-			if(isVowel(c)) {
-				if(pointer == word.length() - 1) {
+			if (isVowel(c)) {
+				if (pointer == word.length() - 1) {
 					enterState();
 				}
 				setState(SINGLEVOWEL);
-			}
-			else if(isLetter(c)) {
+			} else if (isLetter(c)) {
 				setState(CONSONANT);
-			}
-			else {
+			} else {
 				setState(NONWORD);
 			}
 		}
-		
+
 		@Override
 		public void enterState() {
 			syllableCount++;
 		}
-		
+
 	}
 
+	/**
+	 * Single vowel state of the WordCounter.
+	 * 
+	 * @author Sirasath Piyapootinun
+	 *
+	 */
 	class SingleVowelState extends State {
 
 		@Override
 		public void handleChar(char c) {
 			if (isVowel(c)) {
-				if(pointer == word.length() - 1) {
+				if (pointer == word.length() - 1) {
 					enterState();
 				}
 				setState(MULTIVOWEL);
-			}
-			else if (isLetter(c)) {
+			} else if (isLetter(c)) {
 				enterState();
 				setState(CONSONANT);
-			}
-			else if (isHyphen(c)) {
-				if(!isE(word.charAt(pointer - 1))) {
+			} else if (isHyphen(c)) {
+				if (!isE(word.charAt(pointer - 1))) {
 					enterState();
 				}
 				setState(HYPHEN);
-			}
-			else {
+			} else {
 				setState(NONWORD);
 			}
 		}
@@ -92,28 +115,30 @@ public class WordCounter {
 		public void enterState() {
 			syllableCount++;
 		}
-
+		
 	}
-	
-	
+
+	/**
+	 * Multiple state of the WordCounter.
+	 * 
+	 * @author Sirasath Piyapootinun
+	 *
+	 */
 	class MultiVowelState extends State {
 
 		@Override
 		public void handleChar(char c) {
-			if(isVowel(c)) {
-				if(pointer == word.length() - 1) {
+			if (isVowel(c)) {
+				if (pointer == word.length() - 1) {
 					enterState();
 				}
-			}
-			else if(isHyphen(c)) {
+			} else if (isHyphen(c)) {
 				enterState();
 				setState(HYPHEN);
-			}
-			else if(isLetter(c)) {
+			} else if (isLetter(c)) {
 				enterState();
 				setState(CONSONANT);
-			}
-			else {
+			} else {
 				setState(NONWORD);
 			}
 		}
@@ -122,27 +147,30 @@ public class WordCounter {
 		public void enterState() {
 			syllableCount++;
 		}
-		
+
 	}
-	
+
+	/**
+	 * Consonant state of the WordCounter.
+	 * 
+	 * @author Sirasath Piyapootinun
+	 *
+	 */
 	class ConsonantState extends State {
 
 		@Override
 		public void handleChar(char c) {
-			if(isVowelOrY(c)) {
-				if(pointer == word.length() - 1 && !isE(c)) {
+			if (isVowelOrY(c)) {
+				if (pointer == word.length() - 1 && !isE(c)) {
 					enterState();
-				}
-				else if(pointer == word.length() - 1 && isE(c) && syllableCount == 0) {
+				} else if (pointer == word.length() - 1 && isE(c) && syllableCount == 0) {
 					enterState();
 				}
 				setState(SINGLEVOWEL);
-			}
-			else if(isHyphen(c)) {
+			} else if (isHyphen(c)) {
 				setState(HYPHEN);
-			}
-			else if(isLetter(c)) {	}
-			else {
+			} else if (isLetter(c)) {
+			} else {
 				setState(NONWORD);
 			}
 		}
@@ -151,24 +179,27 @@ public class WordCounter {
 		public void enterState() {
 			syllableCount++;
 		}
-		
+
 	}
 	
+	/**
+	 * Hyphen state of the WordCounter.
+	 * 
+	 * @author Sirasath Piyapootinun
+	 *
+	 */
 	class HyphenState extends State {
 
 		@Override
 		public void handleChar(char c) {
-			if(isHyphen(c)) {
+			if (isHyphen(c)) {
 				enterState();
 				setState(NONWORD);
-			}
-			else if(isVowel(c)) {
+			} else if (isVowel(c)) {
 				setState(SINGLEVOWEL);
-			}
-			else if(isLetter(c)) {
+			} else if (isLetter(c)) {
 				setState(CONSONANT);
-			}
-			else {
+			} else {
 				setState(NONWORD);
 			}
 		}
@@ -177,20 +208,26 @@ public class WordCounter {
 		public void enterState() {
 			syllableCount = 0;
 		}
-		
+
 	}
 	
+	/**
+	 * Non word state of the WordCounter.
+	 * 
+	 * @author Sirasath Piyapootinun
+	 *
+	 */
 	class NonWordState extends State {
 
 		@Override
 		public void handleChar(char c) {
-			enterState();		
+			enterState();
 		}
 
 		@Override
 		public void enterState() {
 			syllableCount = 0;
 		}
-		
+
 	}
 }
--- syllable-counter/src/ku/util/SimpleSyllableCounter.java
+++ syllable-counter/src/ku/util/SimpleSyllableCounter.java
@@ -1,7 +1,21 @@
 package ku.util;
 
+/**
+ * SimpleSyllableCounter class for counting syllable in a word using State
+ * Design Pattern (non-OO).
+ * 
+ * @author Sirasath Piyapootinun
+ *
+ */
 public class SimpleSyllableCounter {
 
+	/**
+	 * Return the total number of syllables counted in a word.
+	 * 
+	 * @param word
+	 *            is the word to be counted.
+	 * @return the number of syllables in a word.
+	 */
 	public int countSyllables(String word) {
 		int syllables = 0;
 		char letter = ' ';
@@ -10,114 +24,110 @@ public class SimpleSyllableCounter {
 			letter = word.charAt(i);
 			switch (state) {
 			case START:
-				if(isVowel(letter)) {
+				if (isVowel(letter)) {
 					state = State.SINGLE_VOWEL;
-				}
-				else if(Character.isLetter(letter)) {
+				} else if (Character.isLetter(letter)) {
 					state = State.CONSONANT;
-				}
-				else {
+				} else {
 					state = State.NONWORD;
 				}
 				break;
-				
+
 			case SINGLE_VOWEL:
-				if(isVowel(letter)) {
+				if (isVowel(letter)) {
 					state = State.MULTI_VOWEL;
-					if(i == word.length() - 1) {
+					if (i == word.length() - 1) {
 						syllables++;
 					}
-				}
-				else if(Character.isLetter(letter)) {
+				} else if (Character.isLetter(letter)) {
 					syllables++;
 					state = State.CONSONANT;
-				}
-				else if(letter == '-') {
+				} else if (letter == '-') {
 					state = State.HYPHEN;
-					if(word.charAt(i - 1) != 'e') {
+					if (word.charAt(i - 1) != 'e') {
 						syllables++;
 					}
-				}
-				else {
+				} else {
 					state = State.NONWORD;
 				}
 				break;
-				
+
 			case MULTI_VOWEL:
-				if(isVowel(letter)) {
+				if (isVowel(letter)) {
 					state = State.MULTI_VOWEL;
-					if(i == word.length() - 1) {
+					if (i == word.length() - 1) {
 						syllables++;
 					}
-				}
-				else if(letter == '-') {
+				} else if (letter == '-') {
 					syllables++;
 					state = State.HYPHEN;
-				}
-				else if(Character.isLetter(letter)){
+				} else if (Character.isLetter(letter)) {
 					syllables++;
 					state = State.CONSONANT;
-				}
-				else {
+				} else {
 					state = State.NONWORD;
 				}
 				break;
-				
+
 			case CONSONANT:
-				if(isVowelOrY(letter)) {
+				if (isVowelOrY(letter)) {
 					state = State.SINGLE_VOWEL;
-					if(i == word.length() - 1 && letter != 'e' ) {
+					if (i == word.length() - 1 && letter != 'e') {
 						syllables++;
 					}
-				}
-				else if(letter == '-') {
+				} else if (letter == '-') {
 					state = State.HYPHEN;
-				}
-				else if(Character.isLetter(letter)){
+				} else if (Character.isLetter(letter)) {
 					state = State.CONSONANT;
-				}
-				else {
+				} else {
 					state = State.NONWORD;
 				}
 				break;
-				
+
 			case HYPHEN:
-				if(letter == '-') {
+				if (letter == '-') {
 					return 0;
-				}
-				else if(isVowel(letter)) {
+				} else if (isVowel(letter)) {
 					state = State.SINGLE_VOWEL;
-				}
-				else if(Character.isLetter(letter)) {
+				} else if (Character.isLetter(letter)) {
 					state = State.CONSONANT;
-				}
-				else {
+				} else {
 					state = State.NONWORD;
 				}
 				break;
-				
+
 			case NONWORD:
 				return 0;
 			}
 		}
 		return syllables;
 	}
-	
+
+	/**
+	 * Check if the letter is a vowel(a,e,i,o,u).
+	 * 
+	 * @param letter
+	 *            is the letter to be checked.
+	 * @return true if letter is a vowel, false otherwise.
+	 */
 	public boolean isVowel(char letter) {
-		if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+		if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
 			return true;
 		}
 		return false;
 	}
-	
+
+	/**
+	 * Check if the letter is a vowel(a,e,i,o,u) or y.
+	 * 
+	 * @param letter
+	 *            is the letter to be checked.
+	 * @return true if letter is a vowel or y, false otherwise.
+	 */
 	public boolean isVowelOrY(char letter) {
-		if(isVowel(letter) || letter == 'y' || letter == 'Y') {
+		if (isVowel(letter) || letter == 'y' || letter == 'Y') {
 			return true;
 		}
 		return false;
-	}
-	public static void main(String[] args) {
-		SimpleSyllableCounter s = new SimpleSyllableCounter();
-		System.out.println(s.countSyllables("the"));
 	}
 }
